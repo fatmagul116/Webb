87deb55a224c64e12c560bddbdc74560b03d8b21
fatmagul116 <fevkaladebirsahis@gmail.com>
2025-12-11 23:19:53 +0300

    Create Trainer.cs

---
+++ Trainer.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProjeOdeviWeb_G231210048.Models
+{
+    public class Trainer
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Antrenör adı zorunludur.")]
+        [Display(Name = "Adı Soyadı")]
+        public string FullName { get; set; }
+
+        [Required(ErrorMessage = "Uzmanlık alanı belirtilmelidir.")]
+        [Display(Name = "Uzmanlık Alanı")]
+        public string Specialization { get; set; } // Örn: Kas geliştirme, Kilo verme
+
+        // Antrenörün müsaitlik saatleri
+        [Required]
+        [Display(Name = "Başlangıç Saati")]
+        [DataType(DataType.Time)]
+        public TimeSpan AvailableFrom { get; set; }
+
+        [Required]
+        [Display(Name = "Bitiş Saati")]
+        [DataType(DataType.Time)]
+        public TimeSpan AvailableTo { get; set; }
+
+        // Eski Hali:
+        // public int GymId { get; set; }
+        // public virtual Gym Gym { get; set; }  // HATA BURADA! (Sistem bunu "dolu gelmeli" sanıyor)
+
+        // YENİ HALİ (Soru işareti ekle):
+        public int GymId { get; set; }      // Bu int olarak kalmalı (Zorunlu)
+        public virtual Gym? Gym { get; set; } // Bu ? almalı (Formda boş gelebilir)
+
+        // Antrenörün verebildiği hizmetler
+        // Eski hali: public virtual ICollection<Service> Services { get; set; }
+        public virtual ICollection<Service>? Services { get; set; }
+    }
+}
