1eb8b6433cad378187850f8ec122e49bc1c59478
fatmagul116 <fevkaladebirsahis@gmail.com>
2025-12-17 21:00:35 +0300

    Create AppUser.cs

---
+++ AppUser.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProjeOdeviWeb_G231210048.Models
+{
+    public class AppUser
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Ad Soyad zorunludur")]
+        [Display(Name = "Ad Soyad")]
+        public string FullName { get; set; }
+
+        [Required(ErrorMessage = "E-Posta zorunludur")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [Display(Name = "Rol")]
+        public string Role { get; set; }
+
+        public double? Height { get; set; }
+        public double? Weight { get; set; }
+
+        [Display(Name = "Hesap Onaylı mı?")]
+        public bool IsApproved { get; set; } = false;
+
+        // --- BU KISMI EKLEMEN GEREKİYOR ---
+        // Bir üyenin birden fazla randevusu olabilir, o yüzden Liste (ICollection) yapıyoruz.
+        public virtual ICollection<Appointment>? Appointments { get; set; }
+        // -----------------------------------
+    }
+}
