2c92041e86ca1343e131ee0adfbec04ea0bac764
fatmagul116 <fevkaladebirsahis@gmail.com>
2025-12-20 23:41:27 +0300

    Create ErorViewModel.cs

---
+++ ErorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProjeOdeviWeb_G231210048.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
